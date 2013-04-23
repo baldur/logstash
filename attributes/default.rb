4a3aeb7881378016e637ec6a3096ba7faccf8f63
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-23 16:38:27 -0700

    Updates around server, ruby kibana and beaver usage

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,12 +12,20 @@ default['logstash']['graphite_query'] = "roles:#{node['logstash']['graphite_role
 default['logstash']['elasticsearch_role'] = 'elasticsearch_server'
 default['logstash']['elasticsearch_query'] = "roles:#{node['logstash']['elasticsearch_role']} AND chef_environment:#{node.chef_environment}"
 default['logstash']['elasticsearch_cluster'] = 'logstash'
-default['logstash']['elasticsearch_ip'] = ''
-default['logstash']['elasticsearch_port'] = ''
+default['logstash']['elasticsearch_ip'] = nil
+default['logstash']['elasticsearch_port'] = nil
 default['logstash']['graphite_ip'] = ''
 
 default['logstash']['patterns'] = {}
 default['logstash']['install_zeromq'] = false
+
+default['logstash']['supports_setuid'] = node.platform == 'ubuntu'
+default['logstash']['init_type'] = case node.platform_family
+                                   when 'debian'
+                                     Gem::Version.new(node.platform_version) >= Gem::Version.new('10.04') ? 'upstart' : 'init.d'
+                                   else
+                                     'init.d'
+                                   end
 
 case
 when platform_family?("rhel")
@@ -25,3 +33,7 @@ when platform_family?("rhel")
 when platform_family?("debian")
   node.set['logstash']['zeromq_packages'] = [ "zeromq",  "libzmq-dev"]
 end
+default['logstash']['discovery']['environment_aware'] = true
+default['logstash']['discovery']['minimum_response_time_sec'] = false
+default['logstash']['discovery']['remove_self'] = false
+default['logstash']['discovery']['empty_ok'] = false
